7ba21c9742e09030058fdd2e63b74250845c1c1b
“kashsan-jp” <kashsan_jp@yahoo.co.jp>
2023-07-17 17:00:57 +0900

    first

---
+++ lib/sqflite/sqflite.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'sql_helper.dart';
+
+// Flutter SQFLITE CRUD Oparations.mp4
+
+class MySQFlite extends StatefulWidget {
+  const MySQFlite({super.key});
+
+  @override
+  State<MySQFlite> createState() => _MySQFliteState();
+}
+
+class _MySQFliteState extends State<MySQFlite> {
+  List<Map<String, dynamic>> _journals = [];
+
+  bool _isLoading = true;
+
+  void _refreshJournals() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _journals = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshJournals();
+    print("..number of items ${_journals.length}");
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  Future<void> _addItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
+    _refreshJournals();
+    print("..number of item ${_journals.length}");
+  }
+
+  Future<void> _updateItem(int id) async {
+    await SQLHelper.updateItem(
+        id, _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  void _deleteItem(int id) async {
+    await SQLHelper.deleteItem(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Successfully deleted a journal!'),
+    ));
+    _refreshJournals();
+  }
+
+  void _showForm(int? id) async {
+    if (id != null) {
+      final existingJournal =
+          _journals.firstWhere((element) => element['id'] == id);
+
+      _titleController.text = existingJournal['title'];
+      _descriptionController.text = existingJournal['description'];
+    }
+
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (_) => Container(
+              padding: EdgeInsets.only(
+                top: 15,
+                left: 15,
+                right: 15,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(hintText: 'Title'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: const InputDecoration(hintText: 'Description'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (id == null) {
+                        await _addItem();
+                      }
+                      if (id != null) {
+                        await _updateItem(id);
+                      }
+                      // Clear the text fields
+                      _titleController.text = '';
+                      _descriptionController.text = '';
+
+                      // Close the button sheet
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(id == null ? 'Create Now' : 'Update'),
+                  )
+                ],
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('SQFlight'),
+      ),
+      body: ListView.builder(
+        itemCount: _journals.length,
+        itemBuilder: (context, index) => Card(
+          color: Colors.orange[200],
+          margin: const EdgeInsets.all(15),
+          child: ListTile(
+            title: Text(_journals[index]['title']),
+            subtitle: Text(_journals[index]['description']),
+            trailing: SizedBox(
+                width: 100,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () => _showForm(_journals[index]['id']),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => _deleteItem(_journals[index]['id']),
+                    ),
+                  ],
+                )),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add), onPressed: () => _showForm(null)),
+    );
+  }
+}
